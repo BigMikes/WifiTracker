d487d5aaad71dde603b87158e4813164079f491f
BigMikes <giulio.micheloni@gmail.com>
2016-04-08 00:08:42 +0200

    Added functions that handle the wifi scanning

--- app/src/main/java/com/project/mps/wifitracker/MainActivity.java
+++ app/src/main/java/com/project/mps/wifitracker/MainActivity.java
@@ -1,19 +1,31 @@
 package com.project.mps.wifitracker;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MAIN";
     private WifiManager WifiManager;
     private WifiReceiver WifiRec;
-    private final Handler handler= new Handler();
+    private Timer timerTask;
+    private boolean measuring;
+    private int numberOfSamples;
 
 
     @Override
@@ -21,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        measuring = false;
+        numberOfSamples = 10;
         WifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
         WifiRec = new WifiReceiver();
         registerReceiver(WifiRec, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
@@ -28,22 +42,73 @@ public class MainActivity extends AppCompatActivity {
         if(WifiManager.isWifiEnabled() == false)
         {
             WifiManager.setWifiEnabled(true);
-            Toast.makeText(this, "Wifi is off, activating...", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Activating Wifi module", Toast.LENGTH_LONG).show();
         }
 
+        //Set the listener for the start measuring button
+        Button btSave = (Button) findViewById(R.id.button_start);
+        btSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanWifi();
+            }
+        });
     }
 
     @Override
-    protected void onPause()
-    {
+    protected void onPause() {
         unregisterReceiver(WifiRec);
         super.onPause();
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         registerReceiver(WifiRec, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
         super.onResume();
+    }
+
+    public void scanWifi(){
+        measuring = true;
+        timerTask = new Timer();
+        timerTask.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                WifiManager.startScan();
+            }
+        }, 0, 1000);
+    }
+
+    public void stopScan(){
+        measuring = false;
+        timerTask.cancel();
+        timerTask.purge();
+        timerTask = null;
+    }
+
+    class WifiReceiver extends BroadcastReceiver
+    {
+        public void onReceive(Context c, Intent intent)
+        {
+            if(measuring == true){
+                numberOfSamples--;
+                if(numberOfSamples == 0) {
+                    stopScan();
+                }
+            }
+            Long tsLong = System.currentTimeMillis()/1000;
+            String ts = tsLong.toString();
+            String info = "";
+            List<ScanResult> results;
+            results = WifiManager.getScanResults();
+            Log.i(TAG, "Sample: " + numberOfSamples + " TIME: " + ts + " SCAN: \n");
+            for(int i = 0; i < results.size(); i++){
+                info += "BSSID: " + results.get(i).BSSID + " SSID: " + results.get(i).SSID + " Level: "
+                        + results.get(i).level + " Frequency: "  + results.get(i).frequency;
+                Log.i(TAG, info);
+                info = "";
+            }
+            Log.i(TAG, "END");
+
+        }
     }
 }
