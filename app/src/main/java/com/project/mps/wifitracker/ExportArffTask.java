90385ac818b36cbb104b458adc543abc54673fc0
luigiDB <luigi.debianchi@gmail.com>
2016-04-09 16:16:49 +0200

    Prototype methods they can be changed

--- app/src/main/java/com/project/mps/wifitracker/ExportArffTask.java
+++ app/src/main/java/com/project/mps/wifitracker/ExportArffTask.java
@@ -1,11 +1,28 @@
 package com.project.mps.wifitracker;
 
+import android.net.Uri;
 import android.os.AsyncTask;
+
+import java.util.ArrayList;
 
 /**
  * Created by luigi on 09/04/2016.
  */
-/*
-public class ExportArffTask extends AsyncTask {
+
+public class ExportArffTask extends AsyncTask<DbManager, Integer, ArrayList<Uri>>{
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected ArrayList<Uri> doInBackground(DbManager... dbManagers) {
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+    }
+
 }
-*/
