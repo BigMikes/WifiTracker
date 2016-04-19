7be96ceedfce9617cabd1e3416efb9e80413b518
BigMikes <giulio.micheloni@gmail.com>
2016-04-19 13:22:40 +0200

    Added a simple class for handling sockets

---
+++ app/src/main/java/com/project/mps/wifitracker/SocketClient.java
@@ -0,0 +1,93 @@
+package com.project.mps.wifitracker;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.List;
+
+/**
+ * Created by Giulio on 19/04/2016.
+ */
+public class SocketClient {
+    Socket client;
+    String address;
+    int port;
+    private BufferedReader input;
+    private PrintWriter output;
+
+    public SocketClient(String address, int port) {
+        if(address == null || address.isEmpty())
+            return;
+        if(port <= 0)
+            return;
+        this.address = address;
+        this.port = port;
+    }
+
+    public boolean SocketConnect(){
+        try {
+            this.client = new Socket(address,port);
+            output = new PrintWriter(client.getOutputStream(), true);
+            input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public void closeSocket(){
+        try{
+            output.flush();
+            output.close();
+            input.close();
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readLine(){
+        String ret;
+        if(input == null)
+            return null;
+        try {
+            ret = input.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return ret;
+    }
+
+    public boolean sendLine(String line){
+        if(output == null || line == null || line.isEmpty())
+            return false;
+        try {
+            output.println(line);
+        } catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    //The data outcoming format will be "Mac1=power1,Mac2=power2,Mac3=power3"
+    public boolean sendQuery(List<WifiInfo> samples){
+        if(samples == null || samples.isEmpty())
+            return false;
+        String toSend = "";
+        int count = 0;
+        for(WifiInfo entry : samples){
+            toSend += entry.getBssid() + "=" + entry.getRssi();
+            if(count != samples.size() - 1)
+                toSend += ",";
+            count++;
+        }
+        return sendLine(toSend);
+    }
+}
